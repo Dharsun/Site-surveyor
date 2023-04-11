d7a213f051906f8c22040526a304341c61a7ed95
Dharsun <93769857+Dharsun@users.noreply.github.com>
2023-04-11 15:06:58 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
 
-#!/bin/bash
-
+#Installing python3 & pip
 apt-get install python3 -y
 apt-get install pip -y
-
-# Install required Python3 modules
+# Installing required Python3 modules
 pip3 install urllib3
 pip3 install beautifulsoup4
 pip3 install requests
 pip3 install colorama
-
-
-
-# Install prerequisites
+# Installing prerequisites
 sudo apt-get update
 sudo apt-get -y install curl git
-
-# Install Golang
+# Installing Golang
 sudo apt-get -y install golang
-
 go install github.com/tomnomnom/gf@latest
-
+# Making gf as evniroimental variable
 cp ~/go/bin/gf /usr/bin/
-
+# Creating root directory for gf modules
 mkdir ~/.gf
-
+# Installing gf modules
 git clone https://github.com/Sherlock297/gf_patterns.git
-
+# Moving modules to root gf directory
 cd gf_patterns/
-
 cp *.json ~/.gf
-
 gf -list
-
+rm -r gf_patterns
 
